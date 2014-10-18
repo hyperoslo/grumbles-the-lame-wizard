22c0b357595b22973478eece9067d222c5d087b2
Sergeev Peter <sergeev.peter@gmail.com>
2014-10-18 20:58:29 +0200

    Update #6: move to current location

--- app/models/location.rb
+++ app/models/location.rb
@@ -13,7 +13,9 @@ class Location < BaseEntity
     if connects_to? next_location_key.to_sym
       next_location = current_location.parent.find_in_tree next_location_key.to_sym
       player.move_node_to(next_location)
-      "Woohoo, you moved, now you are in #{next_location.ids.first}"
+      "Woohoo, you moved, now you are in #{next_location_key}"
+    elsif current_location.ids.include? next_location_key.to_sym
+      "You are already in #{next_location_key}"
     else
       "That location is way too far"
     end
