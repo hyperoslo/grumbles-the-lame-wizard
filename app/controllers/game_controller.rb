65064ed5630ccd50c6b103dbab9435c2eb6d47b2
Sindre Moen <sindrenm@gmail.com>
2014-10-18 14:51:41 +0200

    Add default method implementations to base entity

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,13 +1,25 @@
 class GameController < ApplicationController
+  INTERNAL_METHOD_NAMES = {
+    'SEARCH' => :describe,
+    'GET'    => :pickup,
+    'MOVE'   => :move_to,
+    'COPY'   => :duplicate,
+    "LOCK"   => :lock,
+    'UNLOCK' => :unlock,
+    'MERGE'  => :merge,
+    'REPORT' => :talk_to,
+    'PATCH'  => :patch,
+  }
+
   def handle
-    verb         = params[:verb].to_sym
+    method       = internal_method params[:verb]
     entity       = game.find params[:entity]
     other_entity = game.find params[:other_entity]
 
     @response = if other_entity
-                  entity.send verb, game: game, other: other_entity
+                  entity.send method, game: game, other: other_entity
                 else
-                  entity.send verb, game: game
+                  entity.send method, game: game
                 end
   end
 
@@ -15,5 +27,11 @@ class GameController < ApplicationController
 
   def game
     @game ||= Game.new
+  end
+
+  private
+
+  def internal_method(funny)
+    INTERNAL_METHOD_NAMES.fetch funny
   end
 end
--- app/models/base_entity.rb
+++ app/models/base_entity.rb
@@ -5,4 +5,40 @@ class BaseEntity
     # TODO :: Actually find a child of this entity
     Wizard.new
   end
+
+  def describe(game:)
+    'Nothing to see here. Move along.'
+  end
+
+  def pickup(game:)
+    'I wouldn’t pick that up if I were you.'
+  end
+
+  def move(game:)
+    'You try to move, but you can’t.'
+  end
+
+  def copy(game:)
+    'Last I checked, pirating was illegal.'
+  end
+
+  def lock(game:)
+    'Are you being paranoid?'
+  end
+
+  def unlock(game:)
+    'You’re a wizard, not a lockpicker!'
+  end
+
+  def merge(game:, other:)
+    'Merging that doesn’t even make any sense.'
+  end
+
+  def report(game:)
+    'You report it, but nobody cares.'
+  end
+
+  def patch(game:)
+    'You feel like you don’t need to patch this.'
+  end
 end
