a5afadd466524b7fcef7e890ca35aa91e06699d0
Tim Kurvers <tim@moonsphere.net>
2014-10-18 17:33:15 +0200

    Preliminary implementation of location connections

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,11 @@
 class Location < BaseEntity
+
+  def connections
+    []
+  end
+
+  def move_allowed?(target)
+    true
+  end
+
 end
--- app/models/location/inn.rb
+++ app/models/location/inn.rb
@@ -1,5 +1,9 @@
 class Location::Inn < Location
 
+  def connections
+    [ :courtyard ]
+  end
+
   def describe(game)
     'Beautiful inn, such precious furniture'
   end
