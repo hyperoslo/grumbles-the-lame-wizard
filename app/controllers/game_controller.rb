53b40ccd2092cd7079291c711398a28a76457251
Sindre Moen <sindrenm@gmail.com>
2014-10-18 20:29:00 +0200

    Display better error messages

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -19,22 +19,47 @@ class GameController < ApplicationController
   def handle
     method = internal_method params[:verb]
 
-    case method
-    when :move_to
+    if method == :move_to
       target_location = game.find params[:entity]
-      render text: game.player.send(method, game: game, target: target_location)
-    else
+      text = <<HTML
+<div class="response">
+  #{game.player.send(method, game: game, target: target_location)}
+</div>
+HTML
+
+    elsif method.present?
       entity       = game.player.references game, params[:entity]
       other_entity = game.player.references game, params[:other_entity]
 
-      text = if other_entity
-               entity.send method, game: game, other: other_entity
-             else
-               entity.send method, game: game
-             end
+      if entity.nil?
+        text = <<HTML
+<div class="response error">
+  #{params[:entity].capitalize} does not really exist, it’s all in your mind.
+</div>
+HTML
 
-      render text: text
+      else
+        text = if other_entity
+                 entity.send method, game: game, other: other_entity
+               else
+                 entity.send method, game: game
+               end
+
+        text = <<HTML
+<div class="response">
+  #{text}
+</div>
+HTML
+      end
+    else
+      text = <<HTML
+<div class="response error">
+  You try to #{params[:verb]}, but realize you have no idea how to do that.
+</div>
+HTML
     end
+
+    render text: text
 
     save!
   end
@@ -62,6 +87,6 @@ class GameController < ApplicationController
   end
 
   def internal_method(funny)
-    INTERNAL_METHOD_NAMES.fetch funny
+    INTERNAL_METHOD_NAMES.fetch(funny) { nil }
   end
 end
