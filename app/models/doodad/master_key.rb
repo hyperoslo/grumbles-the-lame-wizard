e59308af5c9d307a52d47108b53e8641c00423e5
Sergeev Peter <sergeev.peter@gmail.com>
2014-10-19 12:14:12 +0200

    Story: road to bank

--- app/models/doodad/master_key.rb
+++ app/models/doodad/master_key.rb
@@ -1,2 +1,12 @@
 class Doodad::MasterKey < Doodad
+  attr_accessor :tried
+
+  def initialize
+    super
+    @tried = false
+  end
+
+  def pick_up
+    '<strong>master_key</strong> is behind <strong>glass_pane</strong>. Now way I can original one, goblin will catch me'
+  end
 end
--- app/models/npc/forged_dwarf.rb
+++ app/models/npc/forged_dwarf.rb
@@ -1,2 +1,8 @@
 class NPC::ForgedDwarf < NPC
+  attr_accessor :happy
+
+  def initialize
+    super
+    @happy = false
+  end
 end
