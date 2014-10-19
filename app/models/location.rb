8dbd9034329a2b708d764b120374b94a64b997ee
Tim Kurvers <tim@moonsphere.net>
2014-10-19 13:03:38 +0200

    Correct inn movement behaviour

--- app/models/location.rb
+++ app/models/location.rb
@@ -13,19 +13,6 @@ class Location < BaseEntity
     connections.include? id
   end
 
-  def move_player_to(next_location_key)
-    current_location = self
-    if connects_to? next_location_key.to_sym
-      next_location = current_location.parent.find_in_tree next_location_key.to_sym
-      player.move_node_to(next_location)
-      "Woohoo, you moved, now you are in #{next_location_key}"
-    elsif current_location.id.eql? next_location_key.to_sym
-      "You are already in #{next_location_key}"
-    else
-      "That location is way too far"
-    end
-  end
-
   def list_connections
     connections.map do |connection|
       " - <strong>#{connection}</strong>"
--- app/models/location/inn.rb
+++ app/models/location/inn.rb
@@ -4,21 +4,16 @@ class Location::Inn < Location
     [ :courtyard ]
   end
 
-  def move_player_to(next_location_key)
+  def move_player_to(next_location)
     current_location = self
 
     door = current_location.find :inn_door
 
-    if current_location.id.eql? next_location_key.to_sym
-      "You are already in #{next_location_key}"
-    elsif door.open.eql? false
+    if door.open.eql? false
       "It seems door is closed, if there is a lock there should be the key"
-    elsif connects_to? next_location_key.to_sym
-      next_location = current_location.parent.find_in_tree next_location_key.to_sym
-      player.move_node_to(next_location)
-      "Woohoo, you moved, now you are in #{next_location_key}"
     else
-      "That location is way too far"
+      player.move_node_to(next_location)
+      "Woohoo, you moved to the #{next_location.id}"
     end
   end
 
