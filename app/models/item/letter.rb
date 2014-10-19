eb49e911a40820fc289796cab1abb2eb8c0ea41a
Tim Kurvers <tim@moonsphere.net>
2014-10-19 20:20:37 +0200

    Correct letter indentation

--- app/models/item/letter.rb
+++ app/models/item/letter.rb
@@ -3,9 +3,11 @@ class Item::Letter < Item
     <<-HTML
       HAI GRUMBLES,
 
-      No doubt you’ve heard of the princess that went missing not long ago. We’ve
-      tracked her GPS-enabled crown to a vault in the castle south of here, but no
-      sane person dares venture there. It might be worth the risk.
+      No doubt you’ve heard of the princess that went missing not long ago.
+
+      We’ve tracked her GPS-enabled crown to a vault in the castle south of here, but no sane person dares venture there.
+
+      It might be worth the risk.
 
       To get started, try the following commands:
 
