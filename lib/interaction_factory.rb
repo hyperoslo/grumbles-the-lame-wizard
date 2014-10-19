d33222c074b652528ba7d9d8494b0a7f4f9a621b
Tim Kurvers <tim@moonsphere.net>
2014-10-19 16:27:51 +0200

    Fix up interaction factory

--- lib/interaction_factory.rb
+++ lib/interaction_factory.rb
@@ -2,31 +2,20 @@ class InteractionFactory
   attr_accessor :object
   attr_accessor :as_hash
 
-
-  # args are objects, not symbols
   def initialize(*args)
     self.as_hash = args.each_with_object({}) do |obj, hsh|
       hsh[obj.class.name.demodulize.underscore] = obj
     end
     class_name = self.as_hash.keys.sort.map(&:classify).join("")
-    class_name = "Interaction::#{class_name}"
-#    if class_exists? class_name
-      self.object = Object.const_get(class_name).new
-#    end
+    class_name = "::Interaction::#{class_name}"
+    @object = class_name.constantize.new rescue nil
   end
 
   def perform(method)
     if object && object.respond_to?(method)
       object.send method, self.as_hash
     else
-      "That objects can not be combined"
+      'Those objects can not be combined.'
     end
-  end
-
-  def class_exists?(class_name)
-    klass = Module.const_get(class_name)
-    return klass.is_a?(Class)
-      rescue NameError
-    return false
   end
 end
